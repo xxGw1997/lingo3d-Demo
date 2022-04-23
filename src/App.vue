203b288fbac6381021e5ec034d8b9674f848aa75
yangwenxuan <540040588@qq.com>
2022-04-23 11:10:47 +0800

    添加场景

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import { World, Cube } from "lingo3d-vue"
+import { World, Model, ThirdPersonCamera, Keyboard, types } from "lingo3d-vue";
+import { ref } from "vue";
+
+const pose = ref<string>("idle");
+const person = ref<types.Model>()
+
+const handleKeyPress = (key: string) => {
+  if(['w','s','a','d'].includes(key)){
+    pose.value = 'walking'
+  }
+
+  if(key === 'w'){
+    person.value?.moveForward(-5)
+  }
+  if(key === 's'){
+    person.value?.moveForward(5)
+  }
+  if(key === 'a'){
+    person.value?.moveRight(5)
+  }
+  if(key === 'd'){
+    person.value?.moveRight(-5)
+  }
+};
+
+const handleKeyUp = (key: string) => {
+  if(key === 'w')
+    pose.value = 'idle'
+};
 </script>
 
 <template>
-  <World>
-    <Cube :animation="{ rotationY: [0, 180, 360] }" />
+  <World default-light="studio" :default-light-scale="3" logarithmic-depth>
+    <Model src="cinema.glb" :scale="10" physics="map" :roughness-factor="0.97"/>
+    <ThirdPersonCamera active mouse-control :inner-z="200">
+      <Model
+        src="person.fbx"
+        ref="person"
+        physics="character"
+        :animations="{ idle: 'idle.fbx', walking: 'Walking.fbx' }"
+        :animation="pose"
+        pbr
+      />
+    </ThirdPersonCamera>
+    <Keyboard @key-press="handleKeyPress" @key-up="handleKeyUp" />
   </World>
 </template>
